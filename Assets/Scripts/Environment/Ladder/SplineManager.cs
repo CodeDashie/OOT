ef951ea7d7a4c85885c73fa677991ed667872849
Shane Hoskin <shoskin@hotmail.com.au>
2022-12-08 23:00:02 +1030

    Spline editor visualization added for ladder

---
+++ Assets/Scripts/Environment/Ladder/SplineManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SplineAI
+{
+    [DefaultExecutionOrder(100)]
+    public class SplineManager : MonoBehaviour
+    {
+        public float NodeCombineDist = 5.0f;
+        public float nodeEditorSize = 0.5f;
+        
+        public struct NodeIndexInSpline
+        {
+            // array of the stacked nodes and their spline and index in the spline
+            public UISpline spline;
+            public int splineIndex;
+        }
+
+        public struct Node
+        {
+            // array of the stacked nodes and their spline and index in the spline
+            public List<NodeIndexInSpline> nodesIndexInSpline;
+            
+            // info on position, connections to other nodes appropriate to this 
+            public Vector3 position;
+            public List<int> connections;
+        }
+
+        [HideInInspector]
+        public List<Node> nodes = new List<Node>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GameObject parent = this.transform.parent.gameObject;
+            UISpline[] UISplines = parent.GetComponentsInChildren<UISpline>();
+
+            foreach (UISpline uISpline in UISplines)
+                AddSpline(uISpline);
+        }
+
+        private Vector3 offsetPos(Vector3 p, float aa)
+        {
+            float x = p.x;
+            float y = p.y;
+            float z = p.z;
+
+            // radius
+            float r = Mathf.Sqrt((x * x) + (z * z));
+
+            // angle
+            float a = Mathf.Atan2(z, x) - (aa * (Mathf.PI / 180));
+
+            x = r * Mathf.Cos(a);
+            z = r * Mathf.Sin(a);
+
+            return new Vector3(x, y, z);
+        }
+
+        void AddSpline(UISpline uISpline)
+        {
+            float a = uISpline.gameObject.transform.eulerAngles.y;
+            int prevIndex = -1;
+
+            int splineIndex = 0;
+            foreach (Vector3 point in uISpline.points)
+            {
+                // attempt to find a duplicate
+                int index = nodes.Count;
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    float dist = Vector3.Distance(offsetPos(point, a) + uISpline.transform.parent.position, nodes[i].position);
+                    if (dist < NodeCombineDist)
+                        index = i;
+                }
+
+                // add a new node if this is unique
+                if (index == nodes.Count)
+                {
+                    Node node = new Node();
+                    node.nodesIndexInSpline = new List<NodeIndexInSpline>();
+                    node.position = offsetPos(point, a) + uISpline.transform.parent.position;
+                    node.connections = new List<int>();
+                    nodes.Add(node);
+                }
+
+                // connect this node here to the previous index if its valid
+                if (prevIndex != -1)
+                {
+                    nodes[index].connections.Add(prevIndex); // make as forwards connection
+                    nodes[prevIndex].connections.Add(index);
+                }
+
+                prevIndex = index;
+
+                // make backwards compatible to the splines of the node and it's index there
+                NodeIndexInSpline n = new NodeIndexInSpline();
+                n.spline = uISpline;
+                n.splineIndex = splineIndex;
+                nodes[index].nodesIndexInSpline.Add(n);
+                
+                // go to next index
+                splineIndex++;
+            }
+        }
+
+        public int GetClosestNode(Vector3 pos)
+        {
+            int closestIndex = 0;
+            float curDistance = Vector3.Distance(pos, nodes[0].position);
+
+            for (int i = 1; i < nodes.Count; i++)
+            {
+                float thisDistance = Vector3.Distance(pos, nodes[i].position);
+                if (thisDistance < curDistance)
+                {
+                    closestIndex = i;
+                    curDistance = thisDistance;
+                }
+            }
+
+            return closestIndex;
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/Ladder/UISpline.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SplineAI
+{
+    public class UISpline : MonoBehaviour
+    {
+        public Vector3[] points;
+
+        public void Start()
+        {
+            raycastPointsToGround();
+        }
+
+        private void raycastPointsToGround()
+        {
+            for (int i = 0; i < points.Length && points.Length > 0; i++)
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(points[i], Vector3.down, out hit))
+                    points[i].y -= hit.distance;
+            }
+        }
+
+        private Vector3 offsetPos(Vector3 p)
+        {
+            float x = p.x;
+            float y = p.y;
+            float z = p.z;
+
+            // radius
+            float r = Mathf.Sqrt((x * x) + (z * z));
+
+            // angle
+            float a = Mathf.Atan2(z, x) - ((transform.parent.transform.eulerAngles.y) * (Mathf.PI / 180));
+
+            x = r * Mathf.Cos(a);
+            z = r * Mathf.Sin(a);
+            
+            return new Vector3(x, y, z);
+        }
+
+        private void OnDrawGizmos()
+        {
+            // loop to draw line from point to point in list
+            Gizmos.color = Color.red;
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                int iNext = (i + 1) % points.Length;
+                Gizmos.DrawLine(transform.parent.transform.position + offsetPos(points[i]) + Vector3.up * 0.1f, transform.parent.transform.position + offsetPos(points[iNext]) + Vector3.up * 0.1f);
+            }
+
+            // loop to draw spheres from points in list
+            Gizmos.color = Color.yellow;
+
+            float nodeEditorSize = 1.0f;
+            if (transform.parent != null && transform.parent.parent != null)
+            {
+                GameObject splineObject = transform.parent.parent.gameObject;
+                if (splineObject.GetComponent<SplineManager>() != null)
+                {
+                    SplineManager splineManager = splineObject.GetComponent<SplineManager>();
+                    nodeEditorSize = splineManager.nodeEditorSize;
+                }
+            }
+            
+            //float radius = 0.25f;
+            for (int i = 0; i < points.Length; i++)
+                Gizmos.DrawSphere(transform.parent.transform.position + offsetPos(points[i]), nodeEditorSize);
+        }
+    }
+}
